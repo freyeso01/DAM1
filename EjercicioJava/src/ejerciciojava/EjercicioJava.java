232182ea34ff08d28c9ef4a0dd866cee9f676c87
examen <examen@examen-PC>
2015-10-07 19:51:08 +0200

    Prueba java

---
+++ EjercicioJava/src/ejerciciojava/EjercicioJava.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciojava;
+
+/**
+ *
+ * @author examen
+ */
+public class EjercicioJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int valor1=20;
+        int valor2=30;
+        int suma=valor1+valor2;
+        
+        System.out.println("El resultado de la suma es: "+suma);
+      
+        // TODO code application logic here
+    }
+    
+}
